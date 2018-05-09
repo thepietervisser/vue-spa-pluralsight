1398b67a5b53176fc7eda58bd01f275d434a5ce9
thepietervisser <33032082+thepietervisser@users.noreply.github.com>
2018-05-09 12:39:21 -0700

    initial commit

---
+++ build/dev-server.js
@@ -0,0 +1,34 @@
+const webpack = require("webpack");
+const clientConfig = require("./webpack.client.config");
+const serverConfig = require('./webpack.server.config')
+const path = require('path')
+const MFS = require('memory-fs')
+
+module.exports = function setupDevServer(app, onUpdate) {
+  clientConfig.entry.app = [
+    "webpack-hot-middleware/client",
+    clientConfig.entry.app
+  ]
+  clientConfig.plugins.push(
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoEmitOnErrorsPlugin()
+  )
+  const clientCompiler = webpack(clientConfig);
+  app.use(
+    require("webpack-dev-middleware")(clientCompiler, {
+      stats: {
+        colors: true
+      }
+    }
+    )
+  )
+  app.use(require("webpack-hot-middleware")(clientCompiler));
+
+  const serverCompiler = webpack(serverConfig)
+  const mfs = new MFS()
+  const outputPath = path.join(serverConfig.output.path, 'server/main.js')
+  serverCompiler.outputFileSystem = mfs
+  serverCompiler.watch({}, () => {
+    onUpdate(mfs.readFileSync(outputPath, 'utf-8'))
+  })
+}
---
+++ src/theme/AppFooter.vue
@@ -0,0 +1,9 @@
+<template>
+  <footer class='footer'>
+      <div class='container'>
+        <div class='content has-text-centered'>
+          This is a link
+        </div>
+      </div>
+    </footer>
+</template>
